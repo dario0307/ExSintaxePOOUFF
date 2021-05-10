4865d4a891a74d9580cbe699e31cbba931eab1c4
Dario Mousinho <69993100+dario0307@users.noreply.github.com>
2021-05-10 20:53:38 -0300

    Add files via upload

---
+++ AvaliacaoIndividual/tacocat/Main.java
@@ -0,0 +1,17 @@
+package tacocat;
+
+import java.io.IOException;
+
+
+public class Main {
+    // Aqui roda o programa
+    public static void main(String[] args){
+        try{
+            Jogo jogo = new Jogo();
+
+        }catch(IOException e){
+            System.out.println("Erro");
+        }
+    }
+     
+}
